6bf92113d9678106765700f8f13a36f163f1e250
yawsinhosseinii <yawsinhosseinii@gmail.com>
2024-09-27 23:45:29 +0330

    mysite

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 let $ = document;
 let loader = $.querySelector(".circles");
 let container = $.querySelector(".container");
-let video = $.querySelector(".video");
-let texts = $.querySelector('.texts')
+let texts = $.querySelector(".texts");
 
 window.addEventListener("load", function () {
   setTimeout(function () {
     loader.style.display = "none";
-    $.body.style.background = "white";
-    container.style.display = "block";
+    $.body.style.background =
+      'url("thousand-sunny-from-one-piece.3840x2160-ezgif.com-video-to-gif-converter.gif")';
+    $.body.style.backgroundSize = "cover";
+    $.body.style.backgroundRepeat = "no-repeat";
+    $.body.style.backgroundPosition = "center";
     texts.style.display = 'block'
-  }, 3000);
+  }, 2500);
 });
